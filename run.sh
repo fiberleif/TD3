04a4e5f7400054cd2e35aa6964b4a7ad72b2b0d7
fiberleif <fiberleif@gmail.com>
2019-04-23 16:40:01 +0800

    4/23/2019: datset class from DDPGfD

--- run.sh
+++ run.sh
@@ -11,19 +11,22 @@ do
 	--policy_name "DDPG" \
 	--env_name "HalfCheetah-v1" \
 	--seed $i \
-	--start_timesteps 10000
+	--start_timesteps 10000 \
+	--expert_path "dataset/half_cheetah.npz"
 
 	python main.py \
 	--policy_name "DDPG" \
 	--env_name "Hopper-v1" \
 	--seed $i \
-	--start_timesteps 1000
+	--start_timesteps 1000 \
+	--expert_path "dataset/hopper.npz"
 
 	python main.py \
 	--policy_name "DDPG" \
 	--env_name "Walker2d-v1" \
 	--seed $i \
 	--start_timesteps 1000
+    --expert_path "dataset/walker.npz"
 
 	python main.py \
 	--policy_name "DDPG" \
@@ -31,5 +34,6 @@ do
 	--seed $i \
 	--start_timesteps 10000 \
 	--max_timesteps 2e6 \
-	--eval_freq 2e3
+	--eval_freq 2e3 \
+	--expert_path "dataset/ant.npz"
 done
