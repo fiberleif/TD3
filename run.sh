03c3446f35898d4cf9c9924f244faf3809c76d1b
fiberleif <fiberleif@gmail.com>
2019-04-23 16:19:36 +0800

    4/23/2019: sh error

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 # Script to reproduce results
 export CUDA_VISIBLE_DEVICES=$1
 echo $CUDA_VISIBLE_DEVICES
-for ((i=0;i<5;i+=1))
+
+SEEDS="0 1 2 3 4"
+for i in $SEEDS
 do
 	python main.py \
 	--policy_name "DDPG" \
