d9e75439f701c1f396e628dd519f4adb0b8dc6f4
fiberleif <fiberleif@gmail.com>
2019-04-23 16:22:34 +0800

    4/23/2019: datset class from DDPGfD

--- mujoco_dset.py
+++ mujoco_dset.py
@@ -5,13 +5,17 @@ the values of each item is a list storing the expert trajectory sequentially
 a transition can be: (data['obs'][t], data['acs'][t], data['obs'][t+1]) and get reward data['rews'][t]
 '''
 
+import logger
 import numpy as np
 
 
 class Dset(object):
-    def __init__(self, inputs, labels, randomize):
+    def __init__(self, inputs, labels, rews, next_obs, dones, randomize):
         self.inputs = inputs
         self.labels = labels
+        self.rews = rews
+        self.next_obs = next_obs
+        self.dones = dones
         assert len(self.inputs) == len(self.labels)
         self.randomize = randomize
         self.num_pairs = len(inputs)
@@ -23,8 +27,6 @@ class Dset(object):
             idx = np.arange(self.num_pairs)
             np.random.shuffle(idx)
             self.inputs = self.inputs[idx, :]
-            if len(self.labels.shape) == 1:
-                self.labels = np.reshape(self.labels, (-1, 1))
             self.labels = self.labels[idx, :]
 
     def get_next_batch(self, batch_size):
@@ -36,17 +38,23 @@ class Dset(object):
         end = self.pointer + batch_size
         inputs = self.inputs[self.pointer:end, :]
         labels = self.labels[self.pointer:end, :]
+        rews = self.rews[self.pointer:end]
+        next_obs = self.next_obs[self.pointer:end]
+        dones = self.dones[self.pointer:end]
         self.pointer = end
-        return inputs, labels
+        return inputs, labels, rews, next_obs, dones
 
 
-class MujocoDset(object):
+class Mujoco_Dset(object):
     def __init__(self, expert_path, train_fraction=0.7, traj_limitation=-1, randomize=True):
         traj_data = np.load(expert_path)
         if traj_limitation < 0:
             traj_limitation = len(traj_data['obs'])
         obs = traj_data['obs'][:traj_limitation]
         acs = traj_data['acs'][:traj_limitation]
+        rews = traj_data['rews'][:traj_limitation]
+        next_obs = traj_data['next_obs'][:traj_limitation]
+        dones = traj_data['dones'][:traj_limitation]
 
         # obs, acs: shape (N, L, ) + S where N = # episodes, L = episode length
         # and S is the environment observation/action space.
@@ -54,9 +62,14 @@ class MujocoDset(object):
         if len(obs.shape[2:]) != 0:
             self.obs = np.reshape(obs, [-1, np.prod(obs.shape[2:])])
             self.acs = np.reshape(acs, [-1, np.prod(acs.shape[2:])])
+            self.next_obs = np.reshape(next_obs, [-1, np.prod(obs.shape[2:])])
         else:
             self.obs = np.vstack(obs)
             self.acs = np.vstack(acs)
+            self.next_obs = np.vstack(next_obs)
+        logger.info('demo_obs.shape:{}'.format(obs.shape))
+        self.rews = np.reshape(rews, [-1, 1])
+        self.dones = np.reshape(dones, [-1, 1])
 
         self.rets = traj_data['ep_rets'][:traj_limitation]
         self.avg_ret = sum(self.rets)/len(self.rets)
@@ -67,25 +80,8 @@ class MujocoDset(object):
         self.num_traj = min(traj_limitation, len(traj_data['obs']))
         self.num_transition = len(self.obs)
         self.randomize = randomize
-
-        self.log_info()
-
-        ### clean dataset
-        clean_obs = []
-        clean_acs = []
-        assert len(self.obs) == len(self.acs)
-        for i in range(len(self.obs)):
-            if np.sum(self.obs[i] ** 2)==0.0 and np.sum(self.acs[i] ** 2)==0.0:
-                print('clean one!')
-            else:
-                clean_obs.append(self.obs[i])
-                clean_acs.append(self.acs[i])
-
-        self.obs = np.array(clean_obs)
-        self.acs = np.array(clean_acs)
-        self.num_transition = len(self.obs)
-        self.dset = Dset(self.obs, self.acs, self.randomize)
-        # for behavior cloning
+        self.dset = Dset(self.obs, self.acs, self.rews, self.next_obs, self.dones, self.randomize)
+        # # for behavior cloning
         # self.train_set = Dset(self.obs[:int(self.num_transition*train_fraction), :],
         #                       self.acs[:int(self.num_transition*train_fraction), :],
         #                       self.randomize)
@@ -95,20 +91,22 @@ class MujocoDset(object):
         self.log_info()
 
     def log_info(self):
-        print("Total trajectorues: %d" % self.num_traj)
-        print("Total transitions: %d" % self.num_transition)
-        print("Average returns: %f" % self.avg_ret)
-        print("Std for returns: %f" % self.std_ret)
+        logger.log("Total trajectorues: %d" % self.num_traj)
+        logger.log("Total transitions: %d" % self.num_transition)
+        logger.log("Average returns: %f" % self.avg_ret)
+        logger.log("Std for returns: %f" % self.std_ret)
 
     def get_next_batch(self, batch_size, split=None):
-        if split is None:
-            return self.dset.get_next_batch(batch_size)
-        elif split == 'train':
-            return self.train_set.get_next_batch(batch_size)
-        elif split == 'val':
-            return self.val_set.get_next_batch(batch_size)
-        else:
-            raise NotImplementedError
+        # if split is None:
+        #     return self.dset.get_next_batch(batch_size)
+        # elif split == 'train':
+        #     return self.train_set.get_next_batch(batch_size)
+        # elif split == 'val':
+        #     return self.val_set.get_next_batch(batch_size)
+        # else:
+        #     raise NotImplementedError
+        assert split is None
+        return self.dset.get_next_batch(batch_size)
 
     def get_all_data(self):
         return self.obs, self.acs, self.rews, self.next_obs, self.dones
@@ -121,7 +119,7 @@ class MujocoDset(object):
 
 
 def test(expert_path, traj_limitation, plot):
-    dset = MujocoDset(expert_path, traj_limitation=traj_limitation)
+    dset = Mujoco_Dset(expert_path, traj_limitation=traj_limitation)
     if plot:
         dset.plot()
 
